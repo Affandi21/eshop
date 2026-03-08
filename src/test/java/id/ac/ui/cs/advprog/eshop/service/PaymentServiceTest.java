6842afcb48383769ef8a9e5a22888f2767066a7c
Affandi21 <affandishafwansoleh@gmail.com>
2026-03-08 22:53:29 +0700

    [GREEN] Implement PaymentService and COD feature

--- src/test/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceTest.java
@@ -3,14 +3,19 @@ package id.ac.ui.cs.advprog.eshop.service;
 import id.ac.ui.cs.advprog.eshop.model.Payment;
 import id.ac.ui.cs.advprog.eshop.repository.PaymentRepository;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 @ExtendWith(MockitoExtension.class)
 class PaymentServiceTest {
