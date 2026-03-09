0e1473639de3fa693f5ce5d071b66c845559678d
Affandi21 <affandishafwansoleh@gmail.com>
2026-03-09 13:31:52 +0700

    [GREEN] Implement PaymentController and payment detail page

--- src/test/java/id/ac/ui/cs/advprog/eshop/controller/OrderControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/OrderControllerTest.java
@@ -1,22 +1,30 @@
 package id.ac.ui.cs.advprog.eshop.controller;
 
+import id.ac.ui.cs.advprog.eshop.service.OrderService;
+import id.ac.ui.cs.advprog.eshop.service.PaymentService;
+
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 
 @WebMvcTest(OrderController.class)
 class OrderControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private OrderService orderService;
+
+    @MockBean
+    private PaymentService paymentService;
 
     @Test
     void testCreateOrderPage() throws Exception {
@@ -25,5 +33,4 @@ class OrderControllerTest {
                 .andExpect(status().isOk());
 
     }
-
 }
--- src/test/java/id/ac/ui/cs/advprog/eshop/controller/PaymentControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/PaymentControllerTest.java
@@ -1,14 +1,21 @@
 package id.ac.ui.cs.advprog.eshop.controller;
 
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+import id.ac.ui.cs.advprog.eshop.service.PaymentService;
+
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import java.util.HashMap;
 
+import static org.mockito.Mockito.when;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(PaymentController.class)
@@ -17,12 +24,23 @@ class PaymentControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private PaymentService paymentService;
+
     @Test
-    void testPaymentDetailPage(){
+    void testPaymentDetailPage() throws Exception {
 
-        mockMvc.perform(get("/payment/detail"))
+        Payment payment = new Payment(
+                "1",
+                "VOUCHER",
+                "SUCCESS",
+                new HashMap<>()
+        );
+
+        when(paymentService.getPayment("1")).thenReturn(payment);
+
+        mockMvc.perform(get("/payment/detail/1"))
                 .andExpect(status().isOk());
-
     }
 
 }
